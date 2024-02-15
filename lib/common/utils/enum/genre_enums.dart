ff6b5411fdb756528cc509796f0e0a01172cd5cb
shin-SeungHun <sng7387@gmail.com>
2024-02-15 19:54:27 +0900

    feat: 영화 genre enum 작성

---
+++ lib/common/utils/enum/genre_enums.dart
@@ -0,0 +1,21 @@
+enum GenreEnums {
+  pop,
+  top,
+  upComing,
+  nowPlaying,
+}
+
+extension GenreEnumsExtension on GenreEnums {
+  String get genre {
+    switch (this) {
+      case GenreEnums.pop:
+        return 'popular';
+      case GenreEnums.top:
+        return 'top_rated';
+      case GenreEnums.upComing:
+        return 'upcoming';
+      case GenreEnums.nowPlaying:
+        return 'now_playing';
+    }
+  }
+}
