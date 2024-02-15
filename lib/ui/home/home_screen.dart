04a9366aeeea7241692aa43418ec138c33baf585
shin-SeungHun <sng7387@gmail.com>
2024-02-15 19:57:20 +0900

    refactor: home_screen에 viewModel 적용하고 데이터 화면에 띄우기

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movify/data/model/movie_item.dart';
+import 'package:flutter_movify/ui/home/home_view_model.dart';
 import 'package:flutter_movify/ui/layout/background_widget.dart';
+import 'package:flutter_movify/ui/layout/image_widget.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,50 +12,54 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Movie Finder'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite_border),
+    return Consumer<HomeViewModel>(
+      builder: (context, viewModel, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Movie Finder'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite_border),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search),
+              ),
+            ],
           ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.search),
+          body: Stack(
+            children: [
+              const BackgroundWidget(),
+              RefreshIndicator(
+                onRefresh: () async {
+                  viewModel.movieItemList;
+                },
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: viewModel.movieItemList.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, // 1 개의 행에 보여줄 item 개수
+                    childAspectRatio: 0.7, // item 의 가로 세로의 비율
+                    mainAxisSpacing: 0, // 수평 Padding
+                    crossAxisSpacing: 0, // 수직 Padding
+                  ),
+                  itemBuilder: (context, index) {
+                    final MovieItem movieItem = viewModel.movieItemList[index];
+                    return Container(
+                      margin: const EdgeInsets.all(8.0),
+                      child: ImageWidget(movieItem: movieItem),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          const BackgroundWidget(),
-          GridView.builder(
-            shrinkWrap: true,
-            itemCount: 8,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-              childAspectRatio: 0.7, //item 의 가로 세로의 비율
-              mainAxisSpacing: 0, //수평 Padding
-              crossAxisSpacing: 0, //수직 Padding
-            ),
-            itemBuilder: (context, index) {
-              return Container(
-                  margin: const EdgeInsets.all(8.0),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(16.0),
-                    child: Image.network(
-                      'https://i.namu.wiki/i/tsdkwuhPrYqpG4ovQ69SUEWqbWc9w0_3UjbB81cNGeBLcl-w4vi8IWcUYFLMb32IcddF2qZ1exIgEfzUouT6iUoe6dDwhcViEeIYKJPkzTlhdTibXdoisaH7YphZWlAGozbcFX2yG8d4_efNMNzL8g.webp',
-                      fit: BoxFit.fitHeight,
-                      width: double.infinity,
-                    ),
-                  ));
-            },
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
