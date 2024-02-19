f15ee8a86f003d91f33044badcedc1acf8d2cdf2
shin-SeungHun <sng7387@gmail.com>
2024-02-19 22:13:47 +0900

    feat: 영화정보 검색 repository 메소드 추가

--- lib/data/repository/movie_repository.dart
+++ lib/data/repository/movie_repository.dart
@@ -2,4 +2,6 @@ import 'package:flutter_movify/data/model/movie_item.dart';
 
 abstract interface class MovieRepository {
   Future<List<MovieItem>> getMovieItems({required String query, required int page});
+  Future<List<MovieItem>> searchMovieItems({required String query});
+
 }
