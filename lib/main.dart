9f44b72214bbb7233da3ee037fe0be45ec5ca747
shin-SeungHun <sng7387@gmail.com>
2024-02-19 15:11:31 +0900

    refactor: 위젯 초기화 메소드 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_movify/router/routes.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
