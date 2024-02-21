685b7cfa85b6f37c95a15929bbee1770fda3a45f
shin-SeungHun <sng7387@gmail.com>
2024-02-22 00:31:02 +0900

    feat: 장르별 영화 화면 추가 추후 작성

---
+++ lib/ui/genre/genre_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_movify/common/utils/enum/genre_enums.dart';
+import 'package:flutter_movify/ui/genre/genre_view_model.dart';
+import 'package:flutter_movify/ui/layout/background_widget.dart';
+import 'package:flutter_movify/ui/layout/movie_grid_view.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:rive/rive.dart';
+
+class GenreScreen extends StatefulWidget {
+  const GenreScreen({super.key});
+
+  @override
+  State<GenreScreen> createState() => _GenreScreenState();
+}
+
+class _GenreScreenState extends State<GenreScreen> with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<GenreViewModel>(
+      builder: (context, viewModel, child) {
+        return Scaffold(
+          appBar: AppBar(
+            leading: const Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                  child: RiveAnimation.asset(
+                    'assets/rive/cat.riv',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ],
+            ),
+            title: const Text('Movie Finder'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite_border),
+              ),
+              IconButton(
+                onPressed: () {
+                  context.push('/search');
+                },
+                icon: const Icon(Icons.search),
+              ),
+            ],
+            bottom: TabBar(
+              controller: _tabController,
+              tabs: const [
+                Tab(text: '인기순'),
+                Tab(text: '평점순'),
+              ],
+            ),
+          ),
+          body: Stack(
+            children: [
+              const BackgroundWidget(),
+              TabBarView(
+                controller: _tabController,
+                children: [
+                  MovieGridView(
+                    scrollController: viewModel.popScrollController,
+                    movieList: viewModel.popMovieList,
+                    onRefresh: () async {
+                      await viewModel.fetchPopMovieInfo(query: GenreEnums.pop.genre, page: 1);
+                    },
+                    genre: GenreEnums.pop.genre,
+                  ),
+                  MovieGridView(
+                    scrollController: viewModel.topScrollController,
+                    movieList: viewModel.topMovieList,
+                    onRefresh: () async {
+                      await viewModel.fetchTopMovieInfo(query: GenreEnums.top.genre, page: 1);
+                    },
+                    genre: GenreEnums.top.genre,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
