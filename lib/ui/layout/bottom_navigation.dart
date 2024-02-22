15ee85ff812899ad31bcca6908742e1882ff7312
shin-SeungHun <sng7387@gmail.com>
2024-02-22 17:02:21 +0900

    style: 주석 제거

--- lib/ui/layout/bottom_navigation.dart
+++ lib/ui/layout/bottom_navigation.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:rive/rive.dart';
 
 class BottomNavigation extends StatefulWidget {
   // child는 StatefulNavigationShell 객체를 참조.
